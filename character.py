efd9470f1b528d04c9961ff40eeb0675b5fc27fa
Alexander Praus <alexander.praus@stud.tu-darmstadt.de>
2019-10-04 00:44:01 +0200

    fixed imports; updated constructor to create sprite for character

--- character.py
+++ character.py
@@ -1,6 +1,14 @@
+import sys, pygame
+
 class Character:
 
-	def __init__ (self, name, position, size):
+	def __init__ (self, name, position, size, imageFile):
 		self.name = name
 		self.position = position
 		self.size = size
+
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load(imageFile)
+		self.image = pygame.transform.scale(self.image, self.size)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = self.position
