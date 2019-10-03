c7efc058facb9e88bd4f902f5181171e54d02d43
Alexander Praus <alexander.praus@stud.tu-darmstadt.de>
2019-10-04 00:44:27 +0200

    changed constructor to use constructor of super class

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
+import sys, pygame
+from character import Character
+
 class Player(Character):
 
-	def __init__ (self, name, position, size):
-		self.name = name
-		self.position = position
-		self.size = size
+	def __init__ (self, name, position, size, imageFile):
+		super(Player, self).__init__(name, position, size, imageFile)
