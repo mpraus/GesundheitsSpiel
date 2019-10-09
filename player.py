c8462c27e21873b1f6ff88d219d3d4886ee66b52
Alexander Praus <alexander.praus@stud.tu-darmstadt.de>
2019-10-10 00:01:34 +0200

    added move function

--- player.py
+++ player.py
@@ -5,3 +5,9 @@ class Player(Character):
 
 	def __init__ (self, name, x_cord, y_cord, size, imageFile):
 		super(Player, self).__init__(name, x_cord, y_cord, size, imageFile)
+
+	def move(self, x, y):
+		self.x_cord += x
+		self.y_cord += y
+		self.rect.left = self.x_cord
+		self.rect.top = self.y_cord
