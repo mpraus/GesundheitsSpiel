0d0fde8a50b5856b38af7913a6d4f39df566498a
Alexander Praus <alexander.praus@stud.tu-darmstadt.de>
2019-10-09 23:16:41 +0200

    removed position attribute and split it into x_cord and y_cord

--- player.py
+++ player.py
@@ -3,5 +3,5 @@ from character import Character
 
 class Player(Character):
 
-	def __init__ (self, name, position, size, imageFile):
-		super(Player, self).__init__(name, position, size, imageFile)
+	def __init__ (self, name, x_cord, y_cord, size, imageFile):
+		super(Player, self).__init__(name, x_cord, y_cord, size, imageFile)
