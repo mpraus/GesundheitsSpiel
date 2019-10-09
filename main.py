5243d5199aeb9e13e65ee9898ce49d43af6d407f
Alexander Praus <alexander.praus@stud.tu-darmstadt.de>
2019-10-10 00:02:00 +0200

    added event handling for arrow key input

--- main.py
+++ main.py
@@ -15,11 +15,21 @@ class Game:
 
 		mainScreen = pygame.display.set_mode(size=self.screenSize)
 		background = Background("images/background_example.png")
-		player = Player("Player1", [50,50], [100, 150], "images/harpy.png")
+		player = Player("Player1", 50, 50, [100, 150], "images/harpy.png")
 
 		while(True):
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT: sys.exit()
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_UP:
+						player.move(0,-5)
+					if event.key == pygame.K_DOWN:
+						player.move(0,5)
+					if event.key == pygame.K_RIGHT:
+						player.move(5,0)
+					if event.key == pygame.K_LEFT:
+						player.move(-5,0)
+				
 
 			mainScreen.fill(self.black)
 			mainScreen.blit(pygame.transform.scale(background.image, self.screenSize), background.rect)
