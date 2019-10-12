c557e618e3be90feb6f3ee22215f8d0bebabb81e
A. Praus <alexander.praus@stud.tu-darmstadt.de>
2019-10-12 20:38:03 +0200

    movement only within window; first version of map array implemented

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import sys, pygame
+import numpy as np
 from background import Background
 from player import Player
 from character import Character
@@ -8,27 +9,36 @@ from character import Character
 class Game:
 
 	black = 0, 0, 0
-	screenSize = (1920,1080)
+	screenSize = (1366,768)
 
 	def __init__(self):
 		pygame.init()
 
 		mainScreen = pygame.display.set_mode(size=self.screenSize)
-		background = Background("images/background_example.png")
-		player = Player("Player1", 50, 50, [100, 150], "images/harpy.png")
+		width, height = mainScreen.get_size()
+		tileSize = 50
+		background = Background("images/background_example.png", height, width,tileSize)
+		player = Player("Player1", 0, 0, (50, 50), "images/harpy.png")
+		background.map_arr[0,0] = player.name
 
 		while(True):
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT: sys.exit()
 				if event.type == pygame.KEYDOWN:
+					x_cord = player.x_cord
+					y_cord = player.y_cord
 					if event.key == pygame.K_UP:
-						player.move(0,-5)
+						if 0 <= y_cord - tileSize :
+							player.move(0,-1, tileSize)
 					if event.key == pygame.K_DOWN:
-						player.move(0,5)
+						if 0 <= y_cord + tileSize * 2 <= height - tileSize:
+							player.move(0,1, tileSize)
 					if event.key == pygame.K_RIGHT:
-						player.move(5,0)
+						if 0 <= x_cord + tileSize <= width - tileSize:
+							player.move(1,0, tileSize)
 					if event.key == pygame.K_LEFT:
-						player.move(-5,0)
+						if 0 <= x_cord - tileSize:
+							player.move(-1,0, tileSize)
 				
 
 			mainScreen.fill(self.black)
