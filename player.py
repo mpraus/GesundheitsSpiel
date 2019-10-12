562d983fefc4f68ae3d1172df53268c5944dd902
A. Praus <alexander.praus@stud.tu-darmstadt.de>
2019-10-12 20:37:18 +0200

    changed move function

--- player.py
+++ player.py
@@ -6,8 +6,8 @@ class Player(Character):
 	def __init__ (self, name, x_cord, y_cord, size, imageFile):
 		super(Player, self).__init__(name, x_cord, y_cord, size, imageFile)
 
-	def move(self, x, y):
-		self.x_cord += x
-		self.y_cord += y
+	def move(self, x, y, tileSize):
+		self.x_cord += (x * tileSize)
+		self.y_cord += (y * tileSize)
 		self.rect.left = self.x_cord
 		self.rect.top = self.y_cord
