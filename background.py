85e646ba37b782b9fbc310893d9e370ebae6dc05
A. Praus <alexander.praus@stud.tu-darmstadt.de>
2020-03-18 22:31:45 +0100

    #7 Map parser implementiert

--- background.py
+++ background.py
@@ -7,6 +7,78 @@ class Background(pygame.sprite.Sprite):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load(imageFile)
 		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = [0,0]import sys, pygame
+import numpy as np
+
+class Background(pygame.sprite.Sprite):
+
+	def __init__(self, imageFile, mapFile):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load(imageFile)
+		self.rect = self.image.get_rect()
 		self.rect.left, self.rect.top = [0,0]
+		self.loadLevel(mapFile)
+
+
+	def loadLevel(self, filename):
+		levelDoc = open(filename)
+		mapLine = []
+		characterLine = []
+		i = 0
+		mapDone = False
+		for line in levelDoc:
+			if line == '\n' or line == '\r\n':
+				mapDone = True
+			if mapDone:
+				characterLine.append(line.split())
+			else:
+				mapLine.append(line)
+
+		self.map_arr = np.zeros((len(mapLine[0].split()), len(mapLine))).astype('U256')
+		i = 0
+		for line in mapLine:
+			line = line.split()
+			j = 0
+			for char in line:
+				self.map_arr[j,i] = line[j]
+				j+=1
+			i+=1
+		print(characterLine)
 		self.map_arr = np.zeros((int(round(width / 50)), int(round(height / 50)))).astype('U256')
-		self.tileSize = tileSize
+		self.tileSize = tileSizeimport sys, pygame
+import numpy as np
+
+class Background(pygame.sprite.Sprite):
+
+	def __init__(self, imageFile, mapFile):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load(imageFile)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = [0,0]
+		self.loadLevel(mapFile)
+
+
+	def loadLevel(self, filename):
+		levelDoc = open(filename)
+		mapLine = []
+		characterLine = []
+		i = 0
+		mapDone = False
+		for line in levelDoc:
+			if line == '\n' or line == '\r\n':
+				mapDone = True
+			if mapDone:
+				characterLine.append(line.split())
+			else:
+				mapLine.append(line)
+
+		self.map_arr = np.zeros((len(mapLine[0].split()), len(mapLine))).astype('U256')
+		i = 0
+		for line in mapLine:
+			line = line.split()
+			j = 0
+			for char in line:
+				self.map_arr[j,i] = line[j]
+				j+=1
+			i+=1
+		print(characterLine)
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Game:
 	screenSize = (1366,768)
 
 	def move(self, character, x, y, tileSize, background):
-		background.map_arr[character.x_cord, character.y_cord] = 0
+		background.map_arr[character.x_cord, character.y_cord] = "."
 		character.x_cord += x
 		character.y_cord += y
 		background.map_arr[character.x_cord, character.y_cord] = character.name
@@ -21,18 +21,20 @@ class Game:
 
 	def __init__(self):
 		pygame.init()
-
-		mainScreen = pygame.display.set_mode(size=self.screenSize)
-		width, height = mainScreen.get_size()
-		tileSize = 50
-		background = Background("images/background_example.png", height, width,tileSize)
-		player = Player("Player1", 0, 0, (background.tileSize, background.tileSize), "images/harpy.png")
-		computerPlayer1 = Character("Computer1", 7*50, 6*50, (background.tileSize, background.tileSize), "images/minotaur.png")
-		background.map_arr[0,0] = player.name
+		mainScreen = pygame.display.set_mode(size=(1024, 768))
+		#width, height = mainScreen.get_size()
+		width, height = (1024, 768)
+		tileSize = 64
+		background = Background("images/TestMap.png", "DevMap")
+		player = Player("Player1", 1, 1, (tileSize, tileSize), "images/harpy.png")
+		computerPlayer1 = Character("Computer1", 7*tileSize, 6*tileSize, (tileSize, tileSize), "images/minotaur.png")
+		#self.loadLevel("DevMap", background)
+		background.map_arr[1,1] = player.name
 		background.map_arr[7,6] = computerPlayer1.name
 		arr_x, arr_y = background.map_arr.shape
-		objects = (player.name, computerPlayer1.name)
-		
+		objects = (player.name, computerPlayer1.name, "#")
+		print(background.map_arr)
+		print(objects)
 		while(True):
 			player_x, player_y = player.getCoordinates()
 			for event in pygame.event.get():
@@ -40,24 +42,27 @@ class Game:
 				if event.type == pygame.KEYDOWN:
 					x_cord = player.x_cord
 					y_cord = player.y_cord
-
+					
 					#movement of player
 					if event.key == pygame.K_UP:
-						if 0 <= y_cord - 1 < arr_y and background.map_arr[x_cord, y_cord - 1] not in objects:
+						print(x_cord, arr_x, background.map_arr[x_cord-1,y_cord])
+						if 0 <= y_cord - 1 and background.map_arr[x_cord, y_cord - 1] not in objects:
 							self.move(player, 0, -1, tileSize, background)
 					if event.key == pygame.K_DOWN:
-						if 0 <= y_cord + 2 < arr_y and background.map_arr[x_cord, y_cord + 1] not in objects:
+						print(x_cord, y_cord + 1, arr_y)
+						if y_cord + 1 < arr_y and background.map_arr[x_cord, y_cord + 1] not in objects:
 							self.move(player, 0, 1, tileSize, background)
 					if event.key == pygame.K_RIGHT:
-						if 0 <= x_cord + 1 < arr_x and background.map_arr[x_cord + 1, y_cord] not in objects:
+						if x_cord + 1 < arr_x and background.map_arr[x_cord + 1, y_cord] not in objects:
 							self.move(player, 1, 0, tileSize, background)
 					if event.key == pygame.K_LEFT:
-						if 0 <= x_cord - 1 < arr_x and background.map_arr[x_cord - 1, y_cord] not in objects:
+						if 0 <= x_cord - 1  and background.map_arr[x_cord - 1, y_cord] not in objects:
 							self.move(player, -1, 0, tileSize, background)
-				
+					print(background.map_arr)
 			mainScreen.fill(self.black)
-			mainScreen.blit(pygame.transform.scale(background.image, self.screenSize), background.rect)
+			mainScreen.blit(pygame.transform.scale(background.image, (width, height)), background.rect)
 			mainScreen.blit(player.image, player.rect)
 			mainScreen.blit(computerPlayer1.image, computerPlayer1.rect)
 			pygame.display.flip()
+			
 Game()
