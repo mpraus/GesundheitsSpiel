9f89e8fc9676f0a72cb1c6c8447ad71d893066a4
Leon Schrabeck <lschrabeck@web.de>
2020-03-28 01:56:53 +0100

    #2 code cleanup

--- background.py
+++ background.py
@@ -3,14 +3,17 @@ import numpy as np
 from character import Character
 
 class Background(pygame.sprite.Sprite):
-	TILESIZE = 64
+	tileSize = 64
 
-	def __init__(self, imageFile, mapFile):
+	def __init__(self, imageFile, mapFile, screenSize):
 		pygame.sprite.Sprite.__init__(self)
 		self.image = pygame.image.load(imageFile)
 		self.rect = self.image.get_rect()
 		self.rect.left, self.rect.top = [0,0]
 		self.loadLevel(mapFile)
+		
+		self.mapSize = (1024, 768)
+
 
 	def move(self, character, vector):
 		newPosition = (character.position[0] + vector[0], character.position[1] + vector[1])
@@ -18,14 +21,13 @@ class Background(pygame.sprite.Sprite):
 			self.tileMap[character.position] = "."
 			self.tileMap[newPosition] = character.name
 			character.position = (newPosition)
-			character.rect.left = character.position[0] * self.TILESIZE
-			character.rect.top = character.position[1] * self.TILESIZE
+			character.rect.left = character.position[0] * self.tileSize
+			character.rect.top = character.position[1] * self.tileSize
 
 	def loadLevel(self, filename):
 		levelDoc = open(filename)
 		mapLine = []
 		characterLine = []
-		i = 0
 		mapDone = False
 		for line in levelDoc:
 			if line == '\n' or line == '\r\n':
@@ -36,22 +38,22 @@ class Background(pygame.sprite.Sprite):
 				mapLine.append(line)
 
 		self.tileMap = np.zeros((len(mapLine[0].split()), len(mapLine))).astype('U256')
-		i = 0
+		y = 0
 		for line in mapLine:
 			line = line.split()
-			j = 0
+			x = 0
 			for char in line:
-				self.tileMap[j,i] = line[j]
-				j+=1
-			i+=1
+				self.tileMap[x, y] = char
+				x+=1
+			y+=1
 
 		self.characterArray = []
 		for line in characterLine:
 			if line[0] == 'player':
-				character = Character(line[1], (int(line[2][1]), int(line[2][3])), self.TILESIZE, line[3])
+				character = Character(line[1], (int(line[2][1]), int(line[2][3])), self.tileSize, line[3])
 				self.player = character
 				self.tileMap[int(line[2][1]), int(line[2][3])] = character
 			elif line[0] == 'npc':
-				character = Character(line[1], (int(line[2][1]), int(line[2][3])), self.TILESIZE, line[3])
+				character = Character(line[1], (int(line[2][1]), int(line[2][3])), self.tileSize, line[3])
 				self.characterArray.append(character)
 				self.tileMap[int(line[2][1]), int(line[2][3])] = character
--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from player import Player
 from character import Character
 
 class Game:
-	BLACK = 0, 0, 0
-
 	def __init__(self):
+		BLACK = 0, 0, 0
 		screenSize = (1024, 768)
 
 		pygame.init()
 		mainScreen = pygame.display.set_mode(screenSize)
-		background = Background("images/TestMap.png", "DevMap")
+		background = Background("images/TestMap.png", "DevMap", screenSize)
 		player = background.player
 		tilesX, tilesY = background.tileMap.shape
 
@@ -20,26 +19,17 @@ class Game:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT: sys.exit()
 				if event.type == pygame.KEYDOWN:
-					playerX = player.position[0]
-					playerY = player.position[1]
-					
+					playerX, playerY = player.position
 					if event.key == pygame.K_UP:
-						if 0 <= playerY - 1:
-							background.move(player, (0, -1))
-						
+						if 0 <= playerY - 1: background.move(player, (0, -1))
 					if event.key == pygame.K_DOWN:
-						if playerY + 1 < tilesY:
-							background.move(player, (0, 1))
-
+						if playerY + 1 < tilesY: background.move(player, (0, 1))
 					if event.key == pygame.K_RIGHT:
-						if playerX + 1 < tilesX:
-							background.move(player, (1, 0))
-
+						if playerX + 1 < tilesX: background.move(player, (1, 0))
 					if event.key == pygame.K_LEFT:
-						if 0 <= playerX - 1:
-							background.move(player, (-1, 0))
+						if 0 <= playerX - 1: background.move(player, (-1, 0))
 			
-			mainScreen.fill(self.BLACK)
+			mainScreen.fill(BLACK)
 			mainScreen.blit(pygame.transform.scale(background.image, screenSize), background.rect)
 			mainScreen.blit(player.image, player.rect)
 			for npc in background.characterArray:
